b744830843345b12f17b2bc90839459e62bac936
unknown <ovidan@ovidan-x1.redmond.corp.microsoft.com>
2013-08-09 00:01:25 -0700

    Added API support for GetRealProblems and GetTrainingProblems

---
+++ Icfpc2013/API/TrainRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Icfpc2013
+{
+    public class TrainRequest
+    {
+        public double? size { get; set; }
+        public string[] operators { get; set; }
+
+        public TrainRequest(double? size, string[] operators)
+        {
+            this.size = size;
+            this.operators = operators;
+        }
+
+        public override string ToString()
+        {
+            return Utils.ToString(this);
+        }
+    }
+}
---
+++ Icfpc2013/API/TrainingProblem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Icfpc2013
+{
+    public class TrainingProblem
+    {
+        public string challenge { get; set; }
+        public string id { get; set; }
+        public double size { get; set; }
+        public string[] operators { get; set; }
+
+        public override string ToString()
+        {
+            return Utils.ToString(this);
+        }
+    }
+}
