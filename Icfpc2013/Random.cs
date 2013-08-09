e566fcdf43e73faa2bbd01e4c7e55e0a78449ba8
U-REDMOND\v-jacosk <v-jacosk@V-JACOSK-LAPTOP.redmond.corp.microsoft.com>
2013-08-09 02:52:56 -0700

    Add ulong random number generation.

---
+++ Icfpc2013/Random.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Icfpc2013
+{
+    class Random
+    {
+        public static System.Random Prng = new System.Random(0);
+
+        public static ulong GetRandomULong()
+        {
+            var buffer = new byte[sizeof(ulong)];
+            Prng.NextBytes(buffer);
+            return BitConverter.ToUInt64(buffer, 0);
+        }
+    }
+}
