61c9a983dba0c05d66622ccea6441bf9daa2594b
Alexander Fedora <alexander.fedora@gmail.com>
2013-08-08 19:02:10 -0700

    adding all ops

---
+++ Icfpc2013/NodeOp1Shr4.cs
@@ -0,0 +1,12 @@
+﻿namespace Icfpc2013
+{
+    internal class NodeOp1Shr4 : NodeOp1
+    {
+        public long Eval()
+        {
+            return Node0.Eval()>>4;
+        }
+
+        public Node Node0 { get; set; }
+    }
+}
---
+++ Icfpc2013/NodeOp2Plus.cs
@@ -0,0 +1,14 @@
+﻿namespace Icfpc2013
+{
+    internal class NodeOp2Plus : NodeOp2
+    {
+        public long Eval()
+        {
+            return Node0.Eval() + Node1.Eval();
+        }
+
+        public Node Node0 { get; set; }
+
+        public Node Node1 { get; set; }
+    }
+}
