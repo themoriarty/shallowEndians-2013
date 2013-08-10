228ff6cd3bea6eaf1d00d9f2feeb0305c18f118f
adelinm <amiloslavov@yahoo.com>
2013-08-09 19:36:13 -0700

    Resolve merge

--- SATGeneratation/Utils.cs
+++ SATGeneratation/Utils.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 
 namespace SATGeneratation
 {
+    using Microsoft.Z3;
+
     class Utils
     {
         public static ArgNode CloneTreeStructure(ArgNode root, Context ctx, string nameSuffix)
@@ -177,73 +179,4 @@ namespace SATGeneratation
         }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        public static SatExpression GetBit(int bitIndex, int opcode, string nodePrefix)
-        {
-            return new PosLeafExpresssion { VarName = nodePrefix + bitIndex };
-        }
-
-        public static AndExpression GetBitsForOpCode(string nodePrefix, int opcode)
-        {
-            return new AndExpression
-            {
-                Exp1 = GetBit(0, opcode, nodePrefix),
-                Exp2 = new AndExpression
-                {
-                    Exp1 = GetBit(1, opcode, nodePrefix),
-                    Exp2 = new AndExpression
-                    {
-                        Exp1 = GetBit(2, opcode, nodePrefix),
-                        Exp2 = GetBit(3, opcode, nodePrefix)
-                    }
-                }
-            };
-        }
-
-        public static SatExpression GetBitsFromNumber(string nodePrefix, ulong number, int curOffset)
-        {
-            string varName = nodePrefix + curOffset;
-            ulong mask = (ulong)0x1 << curOffset;
-            AndExpression res = new AndExpression();
-            if (((mask & number) > 0))
-            {
-                res.Exp1 = new PosLeafExpresssion { VarName = varName };
-            }
-            else
-            {
-                res.Exp1 = new NegLeafExpresssion { VarName = varName };
-            }
-            if (curOffset == 63)
-            {
-
-                mask = mask << 1;
-                if (((mask & number) > 0))
-                {
-                    res.Exp2 = new PosLeafExpresssion { VarName = varName };
-                }
-                else
-                {
-                    res.Exp2 = new NegLeafExpresssion { VarName = varName };
-                }
-            }
-            else
-            {
-                res.Exp2 = GetBitsFromNumber(nodePrefix, number, curOffset + 1);
-            }
-            return res;
-        }
-    }
 }
