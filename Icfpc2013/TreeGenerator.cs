2e7a2398ac7d520f0ce2af8e0cdc8a2ec0552694
unknown <ovidan@ovidan-x1.redmond.corp.microsoft.com>
2013-08-09 22:53:51 -0700

    Removed useless code

--- Icfpc2013/TreeGenerator.cs
+++ Icfpc2013/TreeGenerator.cs
@@ -34,8 +34,6 @@ namespace Icfpc2013
 
         public IEnumerable<Node> GenerateAllPrograms()
         {
-            var ret = new List<Node>();
-
             foreach (var node in ValidNodes)
             {
                 foreach (var subtree in GetNLevelTree(node, MaxDepth))
@@ -53,8 +51,6 @@ namespace Icfpc2013
         {
             if (root is Node0 || root is Node1 || root is NodeId)
             {
-                var ret = new List<Node>();
-                ret.Add(root);
                 yield return root;
                 yield break;
             }
@@ -78,12 +74,10 @@ namespace Icfpc2013
                         }
                     }
 
-                    var ret = new List<Node>();
                     foreach (var subtree in subtrees)
                     {
                         var newRoot = root.Clone();
                         (newRoot as NodeOp1).Node0 = subtree;
-                        ret.Add(newRoot);
                         yield return newRoot;
                     }
                 }
